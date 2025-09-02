98be3b5135ccb4e9f689b2278a67710591beab5d
ichynul <ichynul@163.com>
2025-09-02 18:59:48 +0800

    update version

--- common/Module.php
+++ common/Module.php
@@ -22,7 +22,7 @@ use tpext\cms\common\model\CmsTemplateHtml;
 
 class Module extends baseModule
 {
-    protected $version = '2.0.11';
+    protected $version = '2.0.12';
 
     protected $name = 'tpext.cms';
 
